3fcd24bd997a163dbc30bfc17a04390f75456b17
HFG43 <hernanguemes@hotmail.com>
2024-06-22 19:25:34 -0300

    Feat: add mail message view

--- app/mailers/family_mailer.rb
+++ app/mailers/family_mailer.rb
@@ -1,2 +1,8 @@
 class FamilyMailer < ApplicationMailer
+  default from: 'hernan.f.guemes@gmail.com'
+
+  def send_access_token
+    @family = params[:family]
+    mail(to: @family.family_registration_email, subject: 'Family Organizer: your family Token')
+  end  
 end
