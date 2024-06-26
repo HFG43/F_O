9c7c6382280f436fbd53e97b3a2689b6da497cba
HFG43 <hernanguemes@hotmail.com>
2024-06-25 21:10:06 -0300

    Misc: update patch params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if @user.update(user_params.except(:family_name, :family_token))
         format.html { redirect_to user_url(@user), notice: "User was successfully updated." }
         format.json { render :show, status: :ok, location: @user }
       else
