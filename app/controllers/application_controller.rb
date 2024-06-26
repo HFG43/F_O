57032f4035d8f1033c815674f960d126fc190559
HFG43 <hernanguemes@hotmail.com>
2024-06-26 13:42:37 -0300

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :birthday, :profile_image, :family_name, :family_token, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up) do |user_paramas|
+      user_paramas.permit(:name, :email, :birthday, :profile_image, :family_name, :family_token, :password, :password_confirmation)
+    end
+
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :birthday, :profile_image, :family_name, :family_token, :password, :password_confirmation, :current_password])
+    
     devise_parameter_sanitizer.permit(:sign_in) do |user_params|
       user_params.permit(:name, :email, :password)
     end  
