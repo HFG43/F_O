cd4b9d0c12c6bd7ff995a5a3a63dfec57e3eeee6
HFG43 <hernanguemes@hotmail.com>
2024-06-25 21:23:53 -0300

    Feat: install devise and add permited params in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :birthday, :profile_image, :family_name, :family_token, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :birthday, :profile_image, :family_name, :family_token, :password, :password_confirmation, :current_password])
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:name, :email, :password)
+    end  
+  end
 end
--- db/migrate/20240622210349_create_users.rb
+++ db/migrate/20240622210349_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name, null: false
-      t.string :email
+      t.string :email, null: false, default: ""
       t.date :birthday, null: false
       t.references :family, null: false, foreign_key: true
 
