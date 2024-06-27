78c14c290ee9d4256f7af3421a6dfcedb3dad0dd
HFG43 <hernanguemes@hotmail.com>
2024-06-27 10:02:14 -0300

    Feat: add new session/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,20 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: sessions_params[:email])
+    if user&.authenticate(sessions_params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome, #{user.email}"
+      redirect_to root_path
+    else
+      flash[:error] = "Invalid email or password"
+      redirect_to root_path 
+    end  
+  end
+
+  private
+
+  def sessions_params
+    params.permit(:email, :password)
   end  
 end
