0a620387f5e669c01582e1058684b9c42d4d62e8
HFG43 <hernanguemes@hotmail.com>
2024-07-06 11:57:42 -0300

    Misc: define Home rout name

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  get "familyorganizer", to: "families#index" 
   root "families#index"
 end
